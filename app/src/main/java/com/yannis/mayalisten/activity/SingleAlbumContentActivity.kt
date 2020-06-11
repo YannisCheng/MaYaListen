379f71aa01e13803c778304b5e0b69415298de52
YannisCheng <cwj1714@163.com>
2020-06-11 08:56:15 +0800

    专辑放置2个界面
    Signed-off-by: YannisCheng <cwj1714@163.com>

--- app/src/main/java/com/yannis/mayalisten/activity/SingleAlbumContentActivity.kt
+++ app/src/main/java/com/yannis/mayalisten/activity/SingleAlbumContentActivity.kt
@@ -3,11 +3,13 @@ package com.yannis.mayalisten.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.FragmentTransaction
-import com.yannis.mayalisten.R
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.yannis.mayalisten.base.BaseActivity
 import com.yannis.mayalisten.bean.ItemBean
 import com.yannis.mayalisten.databinding.ActivitySingleAlbumContentBinding
+import com.yannis.mayalisten.fragment.SingleAlbumContentFragment
 import com.yannis.mayalisten.fragment.SingleAlbumEvaluationsFragment
 
 class SingleAlbumContentActivity : BaseActivity() {
@@ -15,7 +17,7 @@ class SingleAlbumContentActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySingleAlbumContentBinding
     private lateinit var itemBean: ItemBean
-    private lateinit var fragmentTransaction: FragmentTransaction
+    private lateinit var mdiator: TabLayoutMediator
 
     companion object {
         fun start(context: Context, itemBean: ItemBean) {
@@ -32,16 +34,44 @@ class SingleAlbumContentActivity : BaseActivity() {
             itemBean = it.getSerializableExtra("item_bean") as ItemBean
         }
         setContentView(binding.root)
-        binding.apply {
-            fragmentTransaction = supportFragmentManager.beginTransaction()
 
-            fragmentTransaction.replace(
-                R.id.fl,
-                //SingleAlbumContentFragment.newInstance(itemBean.albumId, true, itemBean.trackId)
-                SingleAlbumEvaluationsFragment.newInstance(itemBean.albumId)
+        val arrayList: ArrayList<Fragment> = ArrayList()
+        arrayList.add(
+            SingleAlbumContentFragment.newInstance(
+                itemBean.albumId,
+                true,
+                itemBean.trackId
             )
-            fragmentTransaction.commit()
-        }
+        )
+        arrayList.add(SingleAlbumEvaluationsFragment.newInstance(itemBean.albumId))
+        binding.apply {
+            tabLayout.addTab(tabLayout.newTab().setText("节目"))
+            tabLayout.addTab(tabLayout.newTab().setText("评价"))
+            viewPager.adapter = object : FragmentStateAdapter(this@SingleAlbumContentActivity) {
+                override fun getItemCount(): Int {
+                    return 2
+                }
 
+                override fun createFragment(position: Int): Fragment {
+                    return arrayList[position]
+                }
+            }
+        }
+        mdiator = TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                when (position) {
+                    0 -> tab.text = "节目"
+                    1 -> tab.text = "评论"
+                }
+            })
+        mdiator.attach()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mdiator.detach()
     }
 }
