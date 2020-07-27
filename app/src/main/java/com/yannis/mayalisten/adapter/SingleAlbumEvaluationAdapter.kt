5c14ce5b0d48aae5be95b23b88cc508a1f1bd9d2
YannisCheng <cwj1714@163.com>
2020-07-27 11:25:30 +0800

    添加工具库
    Signed-off-by: YannisCheng <cwj1714@163.com>

--- app/src/main/java/com/yannis/mayalisten/adapter/SingleAlbumEvaluationAdapter.kt
+++ app/src/main/java/com/yannis/mayalisten/adapter/SingleAlbumEvaluationAdapter.kt
@@ -1,12 +1,15 @@
 package com.yannis.mayalisten.adapter
 
+import android.os.Build
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.yannis.mayalisten.R
 import com.yannis.mayalisten.bean.Evaluation
+import com.yannis.mayalisten.widget.TextShrinkExpansion
 
 /**
  *
@@ -20,6 +23,7 @@ class SingleAlbumEvaluationAdapter(data: MutableList<Evaluation>?) :
         mLayoutResId = R.layout.item_single_album_evaluation_layout
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun convert(helper: BaseViewHolder?, item: Evaluation?) {
 
         item?.let {
@@ -27,7 +31,8 @@ class SingleAlbumEvaluationAdapter(data: MutableList<Evaluation>?) :
             helper?.let {
                 Glide.with(mContext).load(item.smallHeader)
                     .into(it.getView<ImageView>(R.id.iv_avator))
-                it.getView<TextView>(R.id.tv_content).text = item.content
+                //it.getView<TextView>(R.id.tv_content).text = item.content
+                TextShrinkExpansion(it.getView<TextView>(R.id.tv_content), item.content, 2)
                 it.getView<TextView>(R.id.tv_date).text = item.commentId.toString()
                 it.getView<TextView>(R.id.tv_repet).text = item.replyCount.toString()
                 it.getView<TextView>(R.id.tv_good).text = item.likes.toString()
---
+++ app/src/main/java/com/yannis/mayalisten/widget/TextShrinkExpansion.kt
@@ -0,0 +1,99 @@
+package com.yannis.mayalisten.widget
+
+import android.graphics.Color
+import android.os.Build
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.StaticLayout
+import android.text.TextPaint
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.blankj.utilcode.util.ScreenUtils
+
+
+/**
+ * TextShrinkExpansion 文本的展开收起
+ *
+ * @author  wenjia.Cheng  cwj1714@163.com
+ * @date    2020/7/27
+ */
+@RequiresApi(Build.VERSION_CODES.M)
+class TextShrinkExpansion(
+    private val textView: TextView,
+    private val content: String,
+    private val lines: Int
+) {
+
+    //收起的文字
+    private lateinit var shrinkContent: SpannableString
+
+    //展开的文字
+    private lateinit var newContent: SpannableString
+
+    init {
+        val textPaint: TextPaint = textView.paint
+        val withLine: Int = ScreenUtils.getAppScreenWidth()
+        val staticLayout: StaticLayout = StaticLayout.Builder
+            .obtain(content, 0, content.length, textPaint, withLine)
+            .build()
+
+        if (staticLayout.lineCount > 3) {
+            // 展开文本对应的内容
+            openText()
+
+            // 收起文本对应的内容
+            closeText(staticLayout)
+        } else {
+            textView.text = content
+            textView.setOnClickListener(null)
+        }
+    }
+
+    private fun openText() {
+        val contentAdd = "$content    收起"
+        newContent = SpannableString(contentAdd)
+        newContent.setSpan(
+            ForegroundColorSpan(Color.parseColor("#0079e2")),
+            contentAdd.length - 2,
+            contentAdd.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+
+    private fun closeText(staticLayout: StaticLayout) {
+        // 返回文本中的指定行的开头的偏移量
+        val index = staticLayout.getLineStart(lines)
+
+        // 设置收起文本的最后一行数据
+        val interceptStr: String = "${content.subSequence(0, index - 2)}...更多"
+        shrinkContent = SpannableString(interceptStr)
+        // 设置收起文本的最后一行数据中"...更多"的颜色
+        shrinkContent.setSpan(
+            ForegroundColorSpan(Color.parseColor("#0079e2")),
+            interceptStr.length - 2,
+            interceptStr.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // 展示"收起文本"的内容
+        textView.text = shrinkContent
+        // 默认设置选中
+        textView.isSelected = true
+
+        textView.setOnClickListener {
+            if (it.id == textView.id) {
+                if (it.isSelected) {
+                    //如果是收起的状态
+                    textView.setText(newContent);
+                    textView.isSelected = false;
+                } else {
+                    //如果是展开的状态
+                    textView.setText(shrinkContent);
+                    textView.isSelected = true;
+                }
+            }
+        }
+    }
+
+}
