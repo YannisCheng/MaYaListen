1e0ae5fb959fb437926082aa8c62e0c8ceb54fa5
YannisCheng <cwj1714@163.com>
2020-07-28 19:49:27 +0800

    修改

--- app/src/main/java/com/yannis/mayalisten/net/MaYaApi.kt
+++ app/src/main/java/com/yannis/mayalisten/net/MaYaApi.kt
@@ -19,21 +19,21 @@ interface MaYaApi {
     /**
      * 首页顶部tab数据获取
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("discovery-ranking-web/v3/ranking/AggregateRankFirstPage/1590971595389")
     fun getAggregateRankFirstPage(): Observable<BaseResultBean<ArrayList<AggregateRankFirstPageBean>>>
 
     /**
      * 首页单个tab-> 左侧所有list
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("discovery-ranking-web/v3/ranking/AggregateRankListTabs/1590971595575")
     fun getAggregateRankListTabs(@Query("rankingListId") rankingListId: Int): Observable<BaseResultBean<ArrayList<AggregateRankListTabsBean>>>
 
     /**
      * 首页单个tab -> 左侧单个list -> 所有专辑
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("discovery-ranking-web/v3/ranking/concreteRankList/1590971595956")
     fun getConcreteRankList(
         @Query("categoryId") categoryId: Int,
@@ -46,7 +46,7 @@ interface MaYaApi {
     /**
      * 单张专辑所有内容
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("mobile-album/album/page/ts-1590971941673?ac=WIFI&device=iPhone&isQueryInvitationBrand=true&isVideoAsc=true&pageSize=20&source=0")
     fun getSingleAlbumContent(
         @Query("albumId") albumId: Int,
@@ -57,7 +57,7 @@ interface MaYaApi {
     /**
      * 对专辑自身的评价
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("album-comment-mobile/album/comment/list/query/1591776846264?order=content-score-desc&pageId=1&pageSize=20")
     fun getAlbumEvaluations(
         @Query("albumId") albumId: Int
@@ -66,7 +66,7 @@ interface MaYaApi {
     /**
      * 专辑-单条音频播放-声音内容
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("mobile/track/v2/baseInfo/1591777317565?device=android")
     fun getAlbumPlayEntry(
         @Query("trackId") trackId: Int
@@ -75,7 +75,7 @@ interface MaYaApi {
     /**
      * 专辑-单条音频播放-评价
      */
-    @Headers("baseurl:" + XIMALAY)
+    @Headers("baseurl:$XIMALAY")
     @GET("comment-mobile/v1/track/comment/1591777645643?hotPageId=1&hotPageSize=30&imageViewSize=960&order=0&pageId=2&pageSize=30")
     fun getAlbumPlayEntryEvaluations(
         @Query("trackId") trackId: Int
