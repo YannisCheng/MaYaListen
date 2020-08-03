cbd30dd97f6f8d4632e18203671f76bcf3cbce1c
YannisCheng <cwj1714@163.com>
2020-08-03 22:55:44 +0800

    部分下载界面

---
+++ app/src/main/java/com/yannis/mayalisten/activity/BatchDownloadActivity.kt
@@ -0,0 +1,12 @@
+package com.yannis.mayalisten.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yannis.mayalisten.R
+
+class BatchDownloadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batch_download)
+    }
+}
